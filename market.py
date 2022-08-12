1d587227c702550cc3f8e457c63ae43e29dab1fa
smr <mohammad>
2022-08-12 12:27:13 +0430

    Part 4

--- market.py
+++ market.py
@@ -1,11 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '<h1>Hello world</h1>'
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>This is the about page of {username}</h1>'
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
