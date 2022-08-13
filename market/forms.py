66c689801bcd015d4a0a1e8e2acb4e3418069976
smr <mohammad>
2022-08-13 19:27:38 +0430

    Part 12

--- market/forms.py
+++ market/forms.py
@@ -1,12 +1,24 @@
-from ast import Pass
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired,ValidationError
+from market.models import User
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Username')
-    email = StringField(label='Email')
-    password1 = PasswordField("Password")
-    password2 = PasswordField("Repeat your password")
-    submit = SubmitField(label="Submit")
-    
+    username = StringField(label='Username', validators=[
+                           Length(min=2, max=30), DataRequired()])
+    email = StringField(label='Email', validators=[Email(), DataRequired()])
+    password1 = PasswordField("Password", validators=[
+                              Length(min=6), DataRequired()])
+    password2 = PasswordField("Repeat your password",
+                              validators=[EqualTo("password1"), DataRequired()])
+    submit = SubmitField(label="Create Account")
+
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("Username already exists! Please try a diffrent username .")
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("Email already exists! Please try a diffrent email .")
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template
-from market import app
-from market.models import Item
+from flask import render_template, redirect, url_for, flash,get_flashed_messages
+from market import app, db
+from market.models import Item, User
 from market.forms import RegisterForm
+
 
 @app.route('/')
 @app.route('/home')
@@ -14,7 +15,17 @@ def market_page():
     items = Item.query.all()
     return render_template('market.html', items=items)
 
-@app.route('/register')
+
+@app.route('/register', methods=["GET", "POST"])
 def register_page():
     form = RegisterForm()
-    return render_template('register.html',form=form)
+    if form.validate_on_submit():
+        user = User(username=form.username.data,
+                    email=form.email.data, password_hash=form.password1.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('home_page'))
+    if form.errors != {}:
+        for err in form.errors.values():
+            flash(err,category='danger')
+    return render_template('register.html', form=form)
